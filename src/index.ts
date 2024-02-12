a7b318dfb35add99ced01796362cdb56f3aa55a3
eya46 <644310932@qq.com>
2024-02-12 21:18:25 +0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,53 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Logger } from 'koishi';
 
-export const name = 'b23'
+export const name = 'b23';
 
-export interface Config {}
+export interface Config {
+  defaultNum: number;
+  maxNum: number;
+  api: string;
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  defaultNum: Schema.number().min(1).max(100).step(1).default(10).description('获取热搜的默认数量'),
+  maxNum: Schema.number().min(1).max(100).step(1).default(20).description('获取热搜的最大数量'),
+  api: Schema.string()
+    .default(
+      process.env.KOISHI_ENV === 'browser'
+        ? 'https://b23.api888.top/x/v2/search/trending/ranking'
+        : 'https://app.bilibili.com/x/v2/search/trending/ranking'
+    )
+    .description('api地址'),
+});
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, config: Config) {
+  ctx
+    .command('b23 [num:number]', '获取B站热搜')
+    .alias('B站热搜')
+    .action(async (_, num?) => {
+      let targetNum;
+      if (num === undefined) {
+        targetNum = config.defaultNum;
+      } else if (parseInt(num + '') !== num) {
+        return '请输入整数';
+      } else if (num < 1) {
+        return '热搜数量最小为1';
+      } else {
+        targetNum = Math.min(num, config.maxNum);
+      }
+
+      try {
+        const resp = await fetch(`${config.api}?limit=${targetNum}`);
+        const data = await resp.json();
+        const msgs = ['B站热搜'];
+        const list = data?.data?.list || [];
+        for (let i = 0; i < list.length; i++) {
+          msgs.push(`${i + 1}. ${list[i].keyword}`);
+        }
+        return msgs.join('\n');
+      } catch (e) {
+        ctx.logger('b23').error(e);
+        return '获取热搜失败';
+      }
+    });
 }
